a5aaa604c5e6db157e295f235152c393067e87f8
faith-child <fxchild05@gmail.com>
2020-11-07 19:57:35 +0000

    Made some corrections on index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+    require('config/config.php');
+    require('config/db.php');
+    $query = 'SELECT * FROM posts ORDER BY created_at DESC';
+    $result = mysqli_query($conn, $query);
+    $posts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    mysqli_free_result($result);
+    mysqli_close($conn);
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -13,18 +23,18 @@
         <p class="text-center"> By Faith Child</p>
     </div>
     <form class="" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-        <input type="email" name="email" placeholder="Enter Email">
+        <input type="email" name="email" placeholder="Enter Email" >
         <br>
         <input type="password" name="password" placeholder="Enter Password">
         <br>
         <input type="submit" name="login" value="login">
     </form>
-    <div class = "row">
-    <div class = "col-md-4">
-    <div class = "single-blog">
+   
+        <div class ="row" class ="col-md-4" class ="single-blog">
+            <h1> <?php echo $post['title']; ?></h1>
+            <small>class = "blog-meta">Created on <?php echo $post['created at']; ?></small>
     
-    </div>
-    </div>
-    </div>
+        </div>
+    
 </body>
 </html>
--- login.php
+++ login.php
@@ -2,9 +2,6 @@
     require('config/config.php');
     require('config/db.php');
     
-    
-
-
 
 
 ?>
